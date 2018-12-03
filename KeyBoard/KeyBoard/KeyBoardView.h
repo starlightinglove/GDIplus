b34054ac63b827357f50ed111dd697abbc8ba6fb
JKS <ksjang1234@naver.com>
2018-12-03 16:24:56 +0900

    UPDATE

--- KeyBoard/KeyBoard/KeyBoardView.h
+++ KeyBoard/KeyBoard/KeyBoardView.h
@@ -11,20 +11,26 @@ protected: // serialization에서만 만들어집니다.
 	CKeyBoardView() noexcept;
 	DECLARE_DYNCREATE(CKeyBoardView)
 
-// 특성입니다.
+	// 특성입니다.
 public:
 	CKeyBoardDoc* GetDocument() const;
 
-// 작업입니다.
+	// 작업입니다.
 public:
 
-// 재정의입니다.
+	//방향키로 사각형 움직이기
+/**/
+	int m_xPos, m_yPos;
+	int m_xMax, m_yMax;
+	BOOL m_bFill;
+
+	// 재정의입니다.
 public:
 	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
 	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
 protected:
 
-// 구현입니다.
+	// 구현입니다.
 public:
 	virtual ~CKeyBoardView();
 #ifdef _DEBUG
@@ -34,7 +40,7 @@ public:
 
 protected:
 
-// 생성된 메시지 맵 함수
+	// 생성된 메시지 맵 함수
 protected:
 	DECLARE_MESSAGE_MAP()
 public:
@@ -47,6 +53,8 @@ public:
 
 #ifndef _DEBUG  // KeyBoardView.cpp의 디버그 버전
 inline CKeyBoardDoc* CKeyBoardView::GetDocument() const
-   { return reinterpret_cast<CKeyBoardDoc*>(m_pDocument); }
+{
+	return reinterpret_cast<CKeyBoardDoc*>(m_pDocument);
+}
 #endif
 
